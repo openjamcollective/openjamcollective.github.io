3046e360691c82979d36c692ba4332414c132644
keithdang <keith.dang11@gmail.com>
2023-02-11 17:34:28 -0500

    photos

--- src/lib/data/People.tsx
+++ src/lib/data/People.tsx
@@ -1,7 +1,9 @@
 import * as Module from '../interfaces';
 import * as UtilConst from '../util';
 import kd from '../images/Keith2.jpg';
-import DIANA_PHOTO from '../images/diana.jpg';
+import DIANA_PHOTO from '../images/Diana.jpg';
+import AJ_PHOTO from '../images/AJ.jpg';
+import LUIS_PHOTO from '../images/Luis.jpg';
 export const diana: Module.PersonProps = {
     name: 'Diana Da Silva',
     about: 'Diana Da Silva is a caregiver, activist, migrant justice community organizer, community builder, grant writer, second generation settler, multi disciplined mover, and artist. She likes to spend most of her time dreaming and building a better world where dance is present and vibrant.',
@@ -13,7 +15,8 @@ export const diana: Module.PersonProps = {
 export const luis: Module.PersonProps = {
     name: 'Luis Miguel',
     about: 'Luis Miguel aka “Cheech”, is a seasoned professional street dance instructor, choreographer, street performer, competitive battler, and community leader. He has been active in the street dance community for over 10 years, and one of his goals is to continue to expand street dance culture as a fellow grant recipient of Ontario Community Changemakers!',
-    social: 'cheech1teach1/'
+    social: 'cheech1teach1/',
+    media: LUIS_PHOTO
 }
 
 export const keith: Module.PersonProps = {
@@ -29,7 +32,8 @@ export const aj: Module.PersonProps = {
     name: 'AJ Harper',
     about: 'AJ is an African Canadian street dancer, performer, dance instructor, musician, and aspiring professional artist, specializing in popping and waving. He has been evolving within the street dance community for more than fifteen years, and after excelling in street dance competitions, he now wants to explore Afro-cultural and therapeutic approaches to street dance.',
     fb: 'aj.harper.39',
-    social: 'aaronjharper/'
+    social: 'aaronjharper/',
+    media: AJ_PHOTO
 }
 
 export const peoplelist: Module.PersonProps[] = [keith, diana, luis, aj];
