b0cd570fa37fa26ce2dddb2d4a986ec8e3962a6b
keithdang <keith.dang11@gmail.com>
2025-10-26 23:11:32 -0400

    home scroll

--- src/page/Contact.tsx
+++ src/page/Contact.tsx
@@ -1,22 +1,41 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import iglogo from '../lib/images/ig-white.png';
 import email from '../lib/images/email-white.png';
 import botb from '../lib/images/BOTB_Jam.jpg';
 import discord_logo from '../lib/images/icons8-discord-100.png';
 const Contact:React.FC =() =>{
+      useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      const parallax = document.querySelector('.parallax') as HTMLElement;
+      if (parallax) {
+        parallax.style.backgroundPositionY = `${scrollY * 0.5}px`;
+      }
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
     return(
         <header className="App-header">
             <h4>
             <i>Contact us</i>
         </h4>
+      {/* <div
+        className="parallax"
+        style={{
+          backgroundImage: `url(${botb})`,
+        }}
+      ></div> */}
+        
             {contactInfo()}
-            <img className="contact-image" src={botb}/>  
+        <img className='contact-image' src={botb}></img>    
         </header> 
     )
 }
 
 const contactInfo = () => {
-    return <div className="container pagedisplay content-sec">
+    return <div className="container content-sec">
         
         <div className='logo-info'>
             <a>
--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import botb from '../lib/images/BOTB_Clean.jpg';
 import Collaborators from '../components/about/Collaborators';
 import './Page.css';
 const Home:React.FC =() =>{
+          useEffect(() => {
+        const handleScroll = () => {
+          const scrollY = window.scrollY;
+          const parallax = document.querySelector('.home-section') as HTMLElement;
+          if (parallax) {
+            parallax.style.backgroundPositionY = `${scrollY * 0.2}px`;
+          }
+        };
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+      }, []);
     return(
         <header className="App-header">
             <div className="row home-section"
