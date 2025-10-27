0f7cfd7980ec4dfb555c90e775baf32abc49f8ae
keithdang <keith.dang11@gmail.com>
2025-10-27 13:41:31 -0400

    org format

--- src/page/Organization.tsx
+++ src/page/Organization.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {objectives, orgInfoList} from '../lib/data/Info';
 import OrganizationCard from '../components/about/OrganizationCard';
 import bb4 from "../lib/images/bickfordboogie4.jpg"
@@ -6,9 +6,68 @@ import potb from "../lib/images/potb2.jpg";
 
 const Organization: React.FC = () => {
     const mediaArr: string[] = [bb4, "https://www.youtube.com/watch?v=Xmbe9Y5cNpE",potb];
+    useEffect(() => {
+            const handleScroll = () => {
+              const scrollY = window.scrollY;
+              const parallax = document.querySelector('.org-img') as HTMLElement;
+              if (parallax) {
+                parallax.style.backgroundPositionY = `${scrollY * 0.2}px`;
+              }
+            };
+            window.addEventListener('scroll', handleScroll);
+            return () => window.removeEventListener('scroll', handleScroll);
+          }, []);
   return (
     <header className="App-header">
-      <div className="container pagedisplay content-sec">
+        {aboutInfo()}
+        {/* {aboutInfo2(mediaArr)} */}
+    </header>
+  );
+};
+
+export default Organization;
+
+// helper to safely extract video ID
+function extractYouTubeId(url: string): string {
+  const regex =
+    /(?:youtube\.com\/(?:.*v=|embed\/|v\/)|youtu\.be\/)([^&?\/\s]+)/;
+  const match = url.match(regex);
+  return match ? match[1] : '';
+}
+
+const aboutInfo = () => {
+  return (
+      <div className="container pagedisplay">
+          <OrganizationCard obj={orgInfoList[0]} />
+          <div className="row org-img"
+                  style={{
+                  backgroundImage: `url(${bb4})`,
+                  }}>
+          </div>
+          <OrganizationCard obj={orgInfoList[1]} />
+          <div className="row org-img"
+                  style={{
+                  backgroundImage: `url(${potb})`,
+                  }}>
+          </div>
+          <div className="grid-container2 padVertical">
+            <OrganizationCard obj={orgInfoList[2]} />
+              <iframe
+                src={`https://www.youtube.com/embed/${extractYouTubeId("https://www.youtube.com/watch?v=Xmbe9Y5cNpE")}`}
+                title={`Open Jam Intro`}
+                className="orgVid w-full h-full rounded-2xl shadow-lg"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+          </div>
+        </div>
+  )
+}
+
+const aboutInfo2 = (mediaArr: string[]) => {
+  return(
+        <div className="container pagedisplay">
         <div className="row">
         {orgInfoList.map((info, i) => {
           const media = mediaArr[i];
@@ -58,16 +117,5 @@ const Organization: React.FC = () => {
         })}
         </div>
       </div>
-    </header>
-  );
-};
-
-export default Organization;
-
-// helper to safely extract video ID
-function extractYouTubeId(url: string): string {
-  const regex =
-    /(?:youtube\.com\/(?:.*v=|embed\/|v\/)|youtu\.be\/)([^&?\/\s]+)/;
-  const match = url.match(regex);
-  return match ? match[1] : '';
+  )
 }
